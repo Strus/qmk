578cbb6db349d6cc046ae93906771bb41a5bf119
Adrian Kepka <kepka.adrian@gmail.com>
2024-03-02 19:20:49 +0100

    Modify layout

--- keyboards/sofle/keymaps/strus/keymap.c
+++ keyboards/sofle/keymaps/strus/keymap.c
@@ -10,18 +10,14 @@
 
 #include <stdio.h>
 
-enum my_keycodes {
-    OSALTGR = SAFE_RANGE,
-};
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [0] = LAYOUT(
-QK_GESC,         KC_1, KC_2, KC_3, KC_4, KC_5,                                               KC_6, KC_7,         KC_8,         KC_9,         KC_0,            KC_BSLS,
-QK_GESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                                               KC_Y, KC_U,         KC_I,         KC_O,         KC_P,            KC_EQL,
+QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5,                                           KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS,
+QK_GESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                                           KC_Y, KC_U, KC_I, KC_O, KC_P, KC_EQL,
 // LCTL_T(KC_TAB),  LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G,               KC_H, RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_MINS,
-LCTL_T(KC_TAB),  KC_A, KC_S, KC_D, KC_F, KC_G,               KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_MINS,
-LSFT_T(KC_QUOT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE,                             KC_MSTP, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
+LCTL_T(KC_TAB),  KC_A, KC_S, KC_D, KC_F, KC_G,                                   KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_MINS,
+LSFT_T(KC_BSLS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE,                          KC_MSTP, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_QUOT),
         KC_LPRN, KC_RPRN, KC_LGUI, LT(1,KC_SPC), LCTL_T(KC_ENT),                              LT(2,KC_BSPC), RSFT_T(KC_TAB), KC_RALT, KC_LBRC, KC_RBRC
     ),
 
@@ -29,15 +25,15 @@ LSFT_T(KC_QUOT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE,                         
 KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
 KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
 KC_TAB,  KC_TRNS, KC_LCBR, KC_LBRC, KC_LPRN, KC_TRNS,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,  KC_TRNS,
-KC_TRNS, KC_TRNS, KC_RCBR, KC_RBRC, KC_RPRN, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_LCBR, KC_LBRC, KC_LPRN, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_RPRN, KC_RBRC, KC_RCBR, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_DEL, KC_TRNS, KC_LALT, KC_TRNS, KC_TRNS
     ),
 
         [2] = LAYOUT(
 KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
+KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TRNS,       KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
 KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
-KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_TRNS,          KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_LALT, KC_TRNS, KC_TRNS
     ),
 };
@@ -55,17 +51,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_S:
         case KC_X:
         case KC_Z: {
-            // os_variant_t current_os = detected_host_os();
-            // if (!record->event.pressed || current_os == OS_MACOS || !(get_mods() & MOD_BIT_RALT)) {
-            //     break;
-            // }
-            //
-            // uint8_t new_mods = (get_mods() & ~MOD_BIT_RALT) | MOD_BIT_LALT;
-            // clear_mods();
-            // set_mods(new_mods);
-            // tap_code(keycode);
-            // set_mods((new_mods & ~MOD_BIT_LALT) | MOD_BIT_RALT);
-            //
+            os_variant_t current_os = detected_host_os();
+            if (!record->event.pressed || current_os == OS_MACOS || !(get_mods() & MOD_BIT_RALT)) {
+                break;
+            }
+
+            uint8_t new_mods = (get_mods() & ~MOD_BIT_RALT) | MOD_BIT_LALT;
+            clear_mods();
+            set_mods(new_mods);
+            tap_code(keycode);
+            set_mods((new_mods & ~MOD_BIT_LALT) | MOD_BIT_RALT);
+
             return true;
         }
         default: {
